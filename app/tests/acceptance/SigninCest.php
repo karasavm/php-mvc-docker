c193833157bece39ea0313ce9bd0cbc2539432ae
Michael Karasavvas <karasavm@gmail.com>
2019-01-22 14:30:33 +0200

    codeception docker added. first test added.

--- app/tests/acceptance/SigninCest.php
+++ app/tests/acceptance/SigninCest.php
@@ -9,5 +9,10 @@ class SigninCest
     // tests
     public function signInSuccessfully(AcceptanceTester $I)
     {
+        $I->amOnPage('/login');
+        $I->fillField('Email','averelge@yahoo.gr');
+        $I->fillField('Password','qwerty');
+        $I->click('Submit');
+        $I->see('Hello michael karas !');
     }
 }
